f1b49ff6771584f041a281c3631f3d5774ac9ba9
98carlo <carloch98@gmail.com>
2016-07-12 03:24:32 +0200

    added first code (not actually working)

--- InputLayer.java
+++ InputLayer.java
@@ -1,27 +1,42 @@
 import java.util.ArrayList;
 
 public class InputLayer {
-
 	
+	private int numberOfNeuronsInLayer;
+	private static ArrayList <Neuron> ListOfNeurons;
+	
+	@SuppressWarnings("null")
 	public void initLayer (InputLayer inputLayer){
 		
-	}
+		for (int i = 0; i < numberOfNeuronsInLayer; i++){
+			ArrayList <Double> inputLayerNeurons = null;
+			inputLayerNeurons.add(Math.random());
+		}
+		
+	} 
 	
 	public void printLayer(InputLayer inputLayer){
-		
+		System.out.println("------ InputLayer ------");
+		System.out.println(ListOfNeurons);
 	}
 	
 	public void setNumberOfNeuronsInLayer (int numberOfNeuronsInLayer){
-		
+		this.numberOfNeuronsInLayer = numberOfNeuronsInLayer;
+	}
+	
+	public int getNumberOfNeuronsInLayer(){
+		return numberOfNeuronsInLayer;
 	}
 
 	public void setListOfNeurons (ArrayList<Neuron> teachNeuronsOfLayer) {
-		 
+		 for (int i = 0; i < numberOfNeuronsInLayer; i++){
+			 this.ListOfNeurons.add(i, teachNeuronsOfLayer.get(i));
+		 }
 		
 	}
 
-	public ArrayList<Double> getlistOfNeurons() {
-		return null;
+	public ArrayList<Neuron> getlistOfNeurons() {
+		return this.ListOfNeurons;
 	}
 
 }
