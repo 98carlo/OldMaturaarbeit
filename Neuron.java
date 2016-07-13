9380237c7d8615b87e1722951185aae565480996
98carlo <carloch98@gmail.com>
2016-07-13 02:37:50 +0200

    finished the 3 functions (initNeuron, setListOfWeightIn, setListOfWeightOut)

--- Neuron.java
+++ Neuron.java
@@ -6,33 +6,42 @@ public class Neuron {
 	private ArrayList <Double> ListofWeightOut;
 
 	
-	public double initNeuron()
+	public Double initNeuron()
 	{
-		double initNeuronNumber = Math.random();
-		return initNeuronNumber;
+		Double randomnum = Math.random();
+		ListofWeightIn.add(randomnum);
+		ListofWeightOut.add(randomnum);
+		return randomnum;
 		
 	}
 	
 	public void setListOfWeightIn (ArrayList<Double>listOfWeightIn){
 		//set the WeightsIn equal to the parameters
 		//used to change the Weights
-		this.ListofWeightIn = listOfWeightIn;
+		//ensuring that the current ArrayList in the Neuron-class has a big enough Capacity
+		ListofWeightIn.ensureCapacity(listOfWeightIn.size());
+		ListofWeightIn.addAll(0, listOfWeightIn);
+		
 		
 	}
 	
 	public void setListOfWeightOut(ArrayList <Double> listOfWeightOut){
 		//set the WeightsOut equal to the parameters
 		//used to change the Weights
-		this.ListofWeightOut = listOfWeightOut;
+		//ensuring that the current ArrayList in the Neuron-class has a big enough Capacity
+		ListofWeightOut.ensureCapacity(listOfWeightOut.size());
+		ListofWeightOut.addAll(0, listOfWeightOut);
+		
 	}
 	
 	public ArrayList<Double>getlistOfWeightIn(){
 		//used for getting the current WeightsIn
-		return this.ListofWeightIn;
+		return ListofWeightIn;
 	}
 	
 	public ArrayList<Double>getListOfWeightOut(){
 		//used for getting the current WeightsOut
-		return this.ListofWeightOut;
+		return ListofWeightOut;
 	}
+	
 }
