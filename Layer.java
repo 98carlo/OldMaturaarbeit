96cc854eb01f41129ea5d756b692d5d9716d0fa9
98carlo <carloch98@gmail.com>
2016-07-14 18:21:51 +0200

    filled the functions in, same thing as in the Neuron-class

--- Layer.java
+++ Layer.java
@@ -7,19 +7,26 @@ public class Layer {
 	
 	
 	public ArrayList <Neuron> getListOfNeurons(){
-		
+		//Returns the Layer who was just processed
 		return listofNeurons;	
 	}
 	
 	public void setListOfNeurons(ArrayList<Neuron>listOfNeurons){
-		
+		//same thing as in the Neuron-class
+		//ensures the Capacity by setting it equal to the numberOfNeuronsInLayer
+		//then sets it equal to the parameter by replacing it with it
+		listofNeurons.ensureCapacity(numberOfNeuronsInLayer);
+		listofNeurons.addAll(0, listOfNeurons);
 	}
 	
 	public int getNumberOfNeuronsInLayer(){
+		//returns the number of the Neurons in the specified Layer
 		return numberOfNeuronsInLayer;
 	}
 	
 	public void setNumberOfNeuronsInLayer(int numberOfNeuronsInLayer){
+		//sets the number Of Neurons equal to its parameters
+		this.numberOfNeuronsInLayer = numberOfNeuronsInLayer;
 		
 	}
 
