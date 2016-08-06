597e496fcf140b8d80e258f8d8e508ef7809d9b5
98carlo <carloch98@gmail.com>
2016-08-06 15:46:54 +0200

    first version of the function initLayer

--- OutputLayer.java
+++ OutputLayer.java
@@ -2,10 +2,21 @@
 public class OutputLayer {
 
 	
+	private ArrayList <Neuron> ListOfNeurons = new ArrayList <Neuron> ();
+	private int NumberOfNeuronsInLayer;
+	
 	public OutputLayer initLayer (OutputLayer outputLayer){
-		
+		outputLayer = new OutputLayer();
+		ArrayList <Double> weightOut = new ArrayList <Double> ();
+		for (int i = 0; i < NumberOfNeuronsInLayer; i++){
+			Neuron neuron = new Neuron();
+			double WeightOut = neuron.initNeuron();
+			weightOut.clear();
+			weightOut.add(WeightOut);
+			neuron.setListOfWeightIn(weightOut);
+			ListOfNeurons.add(neuron);
+		}
 		return outputLayer;
-	}
 	
 	public void printLayer (OutputLayer outputLayer){
 		
